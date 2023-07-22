d454d11f81d9db9d8a159aa2956f63a037950b21
j-leidy <istaylowkeyz@gmail.com>
2023-07-22 16:48:55 -0500

    providers added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { ThemeProvider } from "styled-components";
+import { MainPage } from "./Pages/MainPage";
+import { selectActiveTheme } from "./Redux/ThemeSlice/ThemeSlice";
+import { useAppSelector } from "./Hooks/Hooks";
 function App() {
-    return <>test</>;
+    const currentTheme = useAppSelector(selectActiveTheme);
+    return (
+        <ThemeProvider theme={currentTheme}>
+            <MainPage />
+        </ThemeProvider>
+    );
 }
 
 export default App;
---
+++ src/Pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+const MainPageBody = styled.div`
+    color: black;
+`;
+
+export const MainPage = () => {
+    return <MainPageBody>test</MainPageBody>;
+};
