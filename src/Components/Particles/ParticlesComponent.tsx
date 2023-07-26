77f3177c102e193fa13dc134fe283ab0cdfdd5c4
j-leidy <istaylowkeyz@gmail.com>
2023-07-25 23:32:15 -0500

    test

--- src/Components/Particles/ParticlesComponent.tsx
+++ src/Components/Particles/ParticlesComponent.tsx
@@ -14,7 +14,7 @@ export const ParticlesComponent = () => {
             id="tsparticles"
             init={customInit}
             width="100vw"
-            height="100%"
+            height={`${window.outerHeight}px`}
             options={theme.particlesOptions}
         />
     );
